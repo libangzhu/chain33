eabad6bb91addee5d924a43433e85f3340e46008
mesment <mesment7826@gmail.com>
2022-04-12 15:31:33 +0800

    添加测试

--- rpc/ethrpc/personal/personal.go
+++ rpc/ethrpc/personal/personal.go
@@ -20,28 +20,6 @@ func NewPersonalApi( cfg *ctypes.Chain33Config,c queue.Client,api client.QueuePr
 	return p
 }
 
-/**
-#personal_listAccounts
-
-req
-
-```
-{"jsonrpc":"2.0","method": "personal_listAccounts", "params": [],"id":1}
-```
-
-response
-
-```
-{
-    "jsonrpc": "2.0",
-    "id": 1,
-    "result": [
-        "19B5wna26upMNHpRf8vMeDMEDx3bkx72be",
-        "1AGZT88duvemtxG1Fd37PcsEyRkt3gi5GP"
-    ]
-}
-```
- */
 func (p *PersonalApi) ListAccounts() ([]string, error){
 	req := &ctypes.ReqAccountList{WithoutBalance: true}
 	msg,err:= p.cli.ExecWalletFunc("wallet","WalletGetAccountList",req)
@@ -57,30 +35,6 @@ func (p *PersonalApi) ListAccounts() ([]string, error){
 	return accounts, nil
 }
 
-/**
-personal_newAccount
-
-req
-
-```
-{
-  "method":"personal_newAccount",
-  "params":["testLabel",2],
-  "id":1,
-  "jsonrpc":"2.0"
-}
-```
-
-response
-
-```
-{
-    "jsonrpc": "2.0",
-    "id": 1,
-    "result": "0xc2315541874910c419c34F1Fa9c6d0ac4E6159E2"
-}
-```
- */
 func (p *PersonalApi) NewAccount(label string, addrID int32) (string, error){
 
 	req := &ctypes.ReqNewAccount{Label:label, AddressID:addrID}
@@ -92,29 +46,6 @@ func (p *PersonalApi) NewAccount(label string, addrID int32) (string, error){
 	return account.Acc.Addr, nil
 }
 
-/**
-# personal_unlockAccount
-
-req
-
-```
-{
-  "method":"personal_unlockAccount",
-  "params":["1AGZT88duvemtxG1Fd37PcsEyRkt3gi5GP", "bty123456",10000],
-  "id":1,
-  "jsonrpc":"2.0"
-}
-```
-
-response
-
-```
-{
-    "jsonrpc": "2.0",
-    "id": 1,
-    "result": true
-}
- */
 func (p *PersonalApi)UnlockAccount(account, passwd  string,  duration int64) bool {
 	req := &ctypes.WalletUnLock{Passwd:passwd, Timeout: duration}
 	resp, err := p.cli.ExecWalletFunc("wallet", "WalletUnLock", req)
@@ -125,31 +56,6 @@ func (p *PersonalApi)UnlockAccount(account, passwd  string,  duration int64) boo
 	return result
 }
 
-
-/**
-#personal_importRawKey
-
-req
-
-```
-{
-  "method":"personal_importRawKey",
-  "params":["07b6e23c7d0e9385eafb7bf8127a63340533fbd4b89c**********99ba1f94", "label"],
-  "id":1,
-  "jsonrpc":"2.0"
-}
-
-```
-
-response
-
-```
-{
-    "jsonrpc": "2.0",
-    "id": 1,
-    "result": "12dyEw2De26DZH6bnWg1pNx72FiPirNkTU"
-}
- */
 func (p *PersonalApi)ImportRawKey(keydata, label string) (string, error) {
 	req := &ctypes.ReqWalletImportPrivkey{Privkey: keydata, Label:label}
 	resp, err := p.cli.ExecWalletFunc("wallet", "WalletImportPrivkey", req)
